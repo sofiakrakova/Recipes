d6b9864557720027b1cc9800061efe4c6fabf78e
skrakova <58389215+sofiakrakova@users.noreply.github.com>
2024-06-27 13:34:30 +0300

    Initial Commit

---
+++ RecipePractice/RecipePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipePracticeApp.swift
+//  RecipePractice
+//
+//  Created by Sofia Krakova on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
